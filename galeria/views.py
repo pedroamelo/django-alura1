a8f7d1d4378bb6eacc1a0e7a26332ce89ff1a2d9
Pedro <pedro.almeidaa@hotmail.com>
2023-02-14 14:57:37 -0300

    finalização do projeto

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1>Alura Space</h1><p>Bem-Vindo ao espaço</p>')
+    return render(request, 'galeria/index.html')
 
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
